9fcd19a551976d23275ff544ca59f55a6844bcbb
Vinicius T. Ferreira <vtf.fatec@hotmail.com>
2023-09-22 10:01:20 -0300

    feat: add delete and update service

--- src/main/java/com/bomvizinho/microservice/boundary/controller/dto/servico/idoso/ServicoIdosoDTO.java
+++ src/main/java/com/bomvizinho/microservice/boundary/controller/dto/servico/idoso/ServicoIdosoDTO.java
@@ -2,23 +2,33 @@ package com.bomvizinho.microservice.boundary.controller.dto.servico.idoso;
 
 import com.fasterxml.jackson.annotation.JsonCreator;
 
-public class ServicoDTO {
+public class ServicoIdosoDTO {
 
+    private String numero;
     private String tipoServico;
     private String dataInicio;
     private String dataFim;
     private VoluntarioServicoDTO voluntarioServicoDTO;
 
-    public ServicoDTO() {
+    public ServicoIdosoDTO() {
     }
 
     @JsonCreator
-    public ServicoDTO(String tipoServico, String dataInicio,
-                      String dataFim, VoluntarioServicoDTO voluntarioServicoDTO) {
+    public ServicoIdosoDTO(String numero, String tipoServico, String dataInicio,
+                           String dataFim, VoluntarioServicoDTO voluntarioServicoDTO) {
+        this.numero = numero;
         this.tipoServico = tipoServico;
         this.dataInicio = dataInicio;
         this.dataFim = dataFim;
         this.voluntarioServicoDTO = voluntarioServicoDTO;
+    }
+
+    public String getNumero() {
+        return numero;
+    }
+
+    public void setNumero(String numero) {
+        this.numero = numero;
     }
 
     public String getTipoServico() {
@@ -45,15 +55,16 @@ public class ServicoDTO {
         this.dataFim = dataFim;
     }
 
-    public VoluntarioServicoDTO getVoluntarioDTO() {
+    public VoluntarioServicoDTO getVoluntarioServicoDTO() {
         return voluntarioServicoDTO;
     }
 
-    public void setVoluntarioDTO(VoluntarioServicoDTO voluntarioServicoDTO) {
+    public void setVoluntarioServicoDTO(VoluntarioServicoDTO voluntarioServicoDTO) {
         this.voluntarioServicoDTO = voluntarioServicoDTO;
     }
 
     public static final class Builder {
+        private String numero;
         private String tipoServico;
         private String dataInicio;
         private String dataFim;
@@ -64,6 +75,11 @@ public class ServicoDTO {
 
         public static Builder aServicoDTO() {
             return new Builder();
+        }
+
+        public Builder withNumero(String numero) {
+            this.numero = numero;
+            return this;
         }
 
         public Builder withTipoServico(String tipoServico) {
@@ -81,18 +97,13 @@ public class ServicoDTO {
             return this;
         }
 
-        public Builder withVoluntarioDTO(VoluntarioServicoDTO voluntarioServicoDTO) {
+        public Builder withVoluntarioServicoDTO(VoluntarioServicoDTO voluntarioServicoDTO) {
             this.voluntarioServicoDTO = voluntarioServicoDTO;
             return this;
         }
 
-        public ServicoDTO build() {
-            ServicoDTO servicoDTO = new ServicoDTO();
-            servicoDTO.setTipoServico(tipoServico);
-            servicoDTO.setDataInicio(dataInicio);
-            servicoDTO.setDataFim(dataFim);
-            servicoDTO.setVoluntarioDTO(voluntarioServicoDTO);
-            return servicoDTO;
+        public ServicoIdosoDTO build() {
+            return new ServicoIdosoDTO(numero, tipoServico, dataInicio, dataFim, voluntarioServicoDTO);
         }
     }
 
