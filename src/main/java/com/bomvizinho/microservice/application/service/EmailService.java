fd8a5c4237ad8d2b9ec5df50fa3906b75b89dffa
Vinicius T. Ferreira <vtf.fatec@hotmail.com>
2023-10-02 12:19:05 -0300

    feat: add reset password url

--- src/main/java/com/bomvizinho/microservice/application/service/EmailService.java
+++ src/main/java/com/bomvizinho/microservice/application/service/EmailService.java
@@ -1,11 +1,15 @@
 package com.bomvizinho.microservice.application.service;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
 
 @Service
 public class EmailService {
+
+    @Value(value = "${url.sistema-bomvizinho.alterar.senha}")
+    private String urlSistemaAlterarSenha;
 
     private JavaMailSender mailSender;
 
@@ -16,8 +20,9 @@ public class EmailService {
     public void sendEmail(String to) {
         SimpleMailMessage message = new SimpleMailMessage();
         message.setTo(to);
-        message.setSubject("pedro");
-        message.setText("E-mail para resetar senha");
+        message.setSubject("Redefinir senha");
+        message.setText("Para redefinir sua senha, utilize o seguinte link: " +
+                urlSistemaAlterarSenha);
 
         mailSender.send(message);
     }
